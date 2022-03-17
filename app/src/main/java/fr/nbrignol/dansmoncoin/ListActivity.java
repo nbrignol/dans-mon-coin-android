89f2dd0945a363fee9f381f2feb54c7bec6c0dd2
Nicolas Brignol <nbrignol@octet-studio.com>
2022-03-17 15:01:40 +0100

    list

--- app/src/main/java/fr/nbrignol/dansmoncoin/ListActivity.java
+++ app/src/main/java/fr/nbrignol/dansmoncoin/ListActivity.java
@@ -1,14 +1,28 @@
 package fr.nbrignol.dansmoncoin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/fr/nbrignol/dansmoncoin/RecyclerViewAdapter.java
@@ -0,0 +1,52 @@
+package fr.nbrignol.dansmoncoin;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter {
+    List<String> dataList;
+
+    RecyclerViewAdapter(){
+        dataList = new ArrayList<String>();
+        dataList.add("Le super portail de l'IUT");
+        dataList.add("Le batiment \"informatique\"");
+        dataList.add("Le resto-U");
+        dataList.add("Un autre");
+        dataList.add("Encore un autre");
+        dataList.add("Et encore un autre");
+        dataList.add("Le dernier");
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.recycler_view_item,
+                parent,
+                false
+        );
+
+        return new ViewHolder(view);
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ViewHolder viewHolder = (ViewHolder) holder;
+        viewHolder.getTextView().setText(dataList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+}
