761056c2192420a5fdbaa9a67060fbcf376c1ddb
Nicolas Brignol <nbrignol@octet-studio.com>
2022-03-17 11:32:15 +0100

    MainMenu as fragment

--- app/src/main/java/fr/nbrignol/dansmoncoin/ListActivity.java
+++ app/src/main/java/fr/nbrignol/dansmoncoin/ListActivity.java
@@ -2,20 +2,13 @@ package fr.nbrignol.dansmoncoin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
+        setContentView(R.layout.activity_list);
     }
 }
---
+++ app/src/main/java/fr/nbrignol/dansmoncoin/MainMenu.java
@@ -0,0 +1,56 @@
+package fr.nbrignol.dansmoncoin;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MainMenu#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MainMenu extends Fragment {
+
+    public MainMenu() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button listButton = view.findViewById(R.id.menu_list);
+
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        MainMenu.this.getContext(), ListActivity.class);
+                //intent.putExtra("key", "value");
+                startActivity(intent);
+            }
+        });
+    }
+}
